ff38754ba7b1ff35012759bccf6e0445335a50b8
ricantony6 <ricantony66@gmail.com>
2023-01-08 11:03:27 -0300

    add function

--- script.js
+++ script.js
@@ -5,9 +5,29 @@ let botaoAC = document.querySelector(".js-btn-ac");
 let botaoMaisMenos = document.querySelector(".js-btn-mais-menos");
 let botaoDeletar = document.querySelector(".js-btn-Del");
 
+function adicionarElementoAoInputResultado(numeroDigitado) {
+    verificarSimboloDuplicado(numeroDigitado)
+    if (verificarSimboloInicial(numeroDigitado)) return;
+    elementoResultado.value += numeroDigitado;
+}
 
+function executarCalculo() {
+    try {
+        elementoResultado.value = eval(elementoResultado.value);
+    } catch {
+        alert("algo deu errado. Tente novamente");
+    }
+}
 
+function limparResultado() {
+  elementoResultado.value = "";
+}
 
+function trocarSinalDaConta() {
+  if (Number(elementoResultado.value)) {
+    elementoResultado.value = elementoResultado.value * -1;
+  }
+}
 
 
 
