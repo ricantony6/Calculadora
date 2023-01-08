45e9d2d1a19c719bd308b127559f5a597de6b049
ricantony6 <ricantony66@gmail.com>
2023-01-08 10:53:33 -0300

    add script

---
+++ script.js
@@ -0,0 +1,15 @@
+let elementoResultado = document.querySelector(".js-resultado");
+let botoesPadroes = document.querySelector(".js-btn-padroes");
+let botaoresultado = document.querySelector(".js-btn-igual");
+let botaoAC = document.querySelector(".js-btn-ac");
+let botaoMaisMenos = document.querySelector(".js-btn-mais-menos");
+let botaoDeletar = document.querySelector(".js-btn-Del");
+
+
+
+
+
+
+
+
+
